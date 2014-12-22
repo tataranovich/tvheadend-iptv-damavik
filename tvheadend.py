1ada78e329a6a1ce2cc6dc7863236667896766e5
Andrey Tataranovich <tataranovich@gmail.com>
2014-12-22 17:14:40 +0300

    Get channel name from tvg-name parameter if exists

--- tvheadend.py
+++ tvheadend.py
@@ -5,17 +5,21 @@ import sys
 import os, os.path
 import json
 import parser
+import re
 
 def configureTvheadend(playlist, outdir):
     id = 0
     for entry in playlist:
         id = id + 1
+        title = entry.title
+        if 'tvg-name=' in entry.length:
+            title = re.search(r'tvg-name="([^"]+)"', entry.length).group(1)
         channel = open(outdir + '/channels/' + str(id), 'w')
-        channel.write(json.dumps({'name': entry.title, 'channel_number': id}, ensure_ascii = False, indent = 4, separators = (',', ': ')))
+        channel.write(json.dumps({'name': title, 'channel_number': id}, ensure_ascii = False, indent = 4, separators = (',', ': ')))
         channel.close()
         service = open(outdir + '/iptvservices/iptv_' + str(id), 'w')
         group, port = entry.path.split('udp://@')[1].split(':', 1)
-        service.write(json.dumps({'pmt':0, 'stype': 1, 'pcr': 0, 'port': port, 'group': group, 'interface': 'eth2', 'channelname': entry.title, 'mapped': 1, 'disabled': 0}, ensure_ascii = False, indent = 4, separators = (',', ': ')))
+        service.write(json.dumps({'pmt':0, 'stype': 1, 'pcr': 0, 'port': port, 'group': group, 'interface': 'eth2', 'channelname': title, 'mapped': 1, 'disabled': 0}, ensure_ascii = False, indent = 4, separators = (',', ': ')))
         service.close()
 
 def showUsage():
