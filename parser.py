1b92441e0edecb683810fecfbd299f1a4fdf47cf
Andrey Tataranovich <tataranovich@gmail.com>
2014-12-23 10:16:24 +0300

    Coding style fixes and refactoring

--- parser.py
+++ parser.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class track():
+
+class Track():
     def __init__(self, length, title, path):
         self.length = length
         self.title = title
         self.path = path 
 
 
-def parseM3U(infile):
+def parse_m3u_file(infile):
     m3u = open(infile, 'r')
+    # TODO: Hardcoded utf-8-sig decoder can misbehave with other M3U files
     line = m3u.readline().decode('utf-8-sig')
     if not line.startswith('#EXTM3U'):
         return None
 
     playlist = []
-    entry = track(None, None, None)
+    entry = Track(None, None, None)
 
     for line in m3u:
         line = line.strip()
         if line.startswith('#EXTINF:'):
             length, title = line.split('#EXTINF:')[1].split(',', 1)
-            entry = track(length.strip(), title.strip(), None)
-        elif (len(line) != 0):
+            entry = Track(length.strip(), title.strip(), None)
+        elif len(line) != 0:
             entry.path = line.strip()
             playlist.append(entry)
-            entry = track(None, None, None)
+            entry = Track(None, None, None)
     m3u.close()
 
     return playlist
--- tvheadend.py
+++ tvheadend.py
@@ -2,45 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os, os.path
+import os
+import os.path
 import json
 import parser
 import re
 
-def configureTvheadend(playlist, outdir):
-    id = 0
+
+def write_json(filename, obj):
+    json_file = open(filename, 'w')
+    json_file.write(json.dumps(obj, ensure_ascii=False, indent=4))
+    json_file.close()
+
+
+def configure_tvheadend(playlist, outdir):
+    i = 0
     for entry in playlist:
-        id = id + 1
+        i += 1
         title = entry.title
         if 'tvg-name=' in entry.length:
             title = re.search(r'tvg-name="([^"]+)"', entry.length).group(1)
-        channel = open(outdir + '/channels/' + str(id), 'w')
-        channel.write(json.dumps({'name': title, 'channel_number': id}, ensure_ascii = False, indent = 4, separators = (',', ': ')))
-        channel.close()
-        service = open(outdir + '/iptvservices/iptv_' + str(id), 'w')
+        channel = {'name': title, 'channel_number': i}
+        write_json(os.path.join(outdir, 'channels', str(i)), channel)
         group, port = entry.path.split('udp://@')[1].split(':', 1)
-        service.write(json.dumps({'pmt':0, 'stype': 1, 'pcr': 0, 'port': port, 'group': group, 'interface': 'eth2', 'channelname': title, 'mapped': 1, 'disabled': 0}, ensure_ascii = False, indent = 4, separators = (',', ': ')))
-        service.close()
+        iptv_service = {'pmt': 0, 'pcr': 0, 'stype': 1, 'port': port, 'group': group, 'interface': 'eth2',
+                        'channelname': title, 'mapped': 1, 'disabled': 0}
+        write_json(os.path.join(outdir, 'iptvservices', 'iptv_' + str(i)), iptv_service)
 
-def showUsage():
-    print 'tvheadend.py <M3Ufile> <OutputDir>'
+
+def show_usage():
+    print 'Usage: tvheadend.py <M3Ufile> <OutputDir>'
+
 
 def main():
+    if len(sys.argv) < 3:
+        show_usage()
+        sys.exit(1)
+
     m3ufile = sys.argv[1]
     outdir = sys.argv[2]
 
-    if len(sys.argv) < 3:
-        showUsage()
+    if not os.path.isfile(m3ufile):
+        print >> sys.stderr, '[Error] Specified M3U file not found: %s' % m3ufile
         sys.exit(1)
-        
     if not os.path.isdir(outdir):
-        print 'Specified output directory not exists: %s' % outdir
+        print >> sys.stderr, '[Error] Specified output directory not found: %s' % outdir
         sys.exit(1)
     else:
-        os.mkdir(outdir + '/channels')
-        os.mkdir(outdir + '/iptvservices')
+        try:
+            os.mkdir(os.path.join(outdir, 'channels'))
+            os.mkdir(os.path.join(outdir, 'iptvservices'))
+        except OSError:
+            print >> sys.stderr, 'Unable to create tvheadend directory structure'
+            sys.exit(1)
 
-    configureTvheadend(parser.parseM3U(m3ufile), outdir)
+    configure_tvheadend(parser.parse_m3u_file(m3ufile), outdir)
 
 if __name__ == '__main__':
     main()
