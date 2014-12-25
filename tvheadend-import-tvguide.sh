8baebcf282a51d0ed995a006838d5f60e399dcaa
Andrey Tataranovich <tataranovich@gmail.com>
2014-12-25 13:22:45 +0300

    Change EPG provider to teleguide.info

--- tvheadend-import-tvguide.sh
+++ tvheadend-import-tvguide.sh
@@ -4,8 +4,8 @@
 # License: GPL-3
 # Website: https://github.com/tataranovich/tvheadend-iptv-damavik
 
-TVGUIDE_URL='http://www.vipiko.tv/get?cur-xtv-tgz'
-TVGUIDE_LOCAL="$HOME/vipiko.tv.tgz"
+TVGUIDE_URL='http://www.teleguide.info/download/new3/xmltv.xml.gz'
+TVGUIDE_LOCAL="$HOME/teleguide.info.xml.gz"
 
 if [ -f "$TVGUIDE_LOCAL" ]; then
     MTIME_OLD=$(stat -c %Y "$TVGUIDE_LOCAL")
@@ -17,5 +17,5 @@ wget -q -N "$TVGUIDE_URL" -O "$TVGUIDE_LOCAL"
 MTIME_NEW=$(stat -c %Y "$TVGUIDE_LOCAL")
 if [ "x$MTIME_OLD" != "x$MTIME_NEW" ]; then
     # File was changed on the server, we need to import it
-    tar -xzOf "$TVGUIDE_LOCAL" | nc -U /home/hts/.hts/tvheadend/epggrab/xmltv.sock
+    gzip -dc "$TVGUIDE_LOCAL" | nc -U /home/hts/.hts/tvheadend/epggrab/xmltv.sock
 fi
