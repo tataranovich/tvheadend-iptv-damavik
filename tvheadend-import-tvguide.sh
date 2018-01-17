c5e66edc67afdeaf1411ebcf212b11325c5ea3ba
Andrey Tataranovich <tataranovich@gmail.com>
2018-01-17 10:26:59 +0300

    Use xmltv instead of jtv.

--- tvheadend-import-tvguide.sh
+++ tvheadend-import-tvguide.sh
@@ -5,9 +5,8 @@
 # Website: https://github.com/tataranovich/tvheadend-iptv-damavik
 
 RUN_PREFIX="$(dirname $0)"
-TVGUIDE_URL='http://help.telecom.by/_files/TelecomTV/TelecomTVepg.zip'
-TVGUIDE_LOCAL="$HOME/TelecomTVepg.zip"
-TVGUIDE_XMLTV="$HOME/TelecomTVepg.xmltv"
+TVGUIDE_URL='http://help.telecom.by/_files/TelecomTV/TelecomTVepg/xmltv.xml'
+TVGUIDE_LOCAL="$HOME/xmltv.xml"
 
 if [ -f "$TVGUIDE_LOCAL" ]; then
     MTIME_OLD=$(stat -c %Y "$TVGUIDE_LOCAL")
@@ -23,6 +22,5 @@ wget -q -N "$TVGUIDE_URL" -O "$TVGUIDE_LOCAL"
 MTIME_NEW=$(stat -c %Y "$TVGUIDE_LOCAL")
 if [ "x$MTIME_OLD" != "x$MTIME_NEW" ]; then
     # File was changed on the server, we need to import it
-    python "$RUN_PREFIX/jtv2xmltv.py" -t +0300 -i "$TVGUIDE_LOCAL" -o "$TVGUIDE_XMLTV"
-    python "$RUN_PREFIX/xmltv-metadata.py" -i "$TVGUIDE_XMLTV" | nc -U /home/hts/.hts/tvheadend/epggrab/xmltv.sock
+    cat "$TVGUIDE_LOCAL" | socat - UNIX-CONNECT:/home/hts/.hts/tvheadend/epggrab/xmltv.sock
 fi
